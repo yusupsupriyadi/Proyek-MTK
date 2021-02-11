646dae64dbc44c77341c774c1f86694a5be9e0e2
bintangyudha203 <bintangyudha203@gmail.com>
2021-02-11 10:25:59 +0700

    update bintang

--- app/Http/Controllers/BintangController.php
+++ app/Http/Controllers/BintangController.php
@@ -10,4 +10,15 @@ class BintangController extends Controller
     {
         return view('backend.bintang.index');
     }
+    public function Proses(Request $request)
+    {
+        $x1 = $request->input('x1');
+        $y1 = $request->input('y1');
+        $r2 = $request->input('z2');
+
+        $hasil = " ($x1 * $x1) + ($y1 * $y1) = 0 ";
+
+
+        return redirect('/bintang')->with('info', 'Hasil nya adalah :' . $hasil);
+    }
 }
