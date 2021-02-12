255ca85d3edefc5f2cfb05adbd7814ffc26be854
bintangyudha203 <bintangyudha203@gmail.com>
2021-02-12 16:22:53 +0700

    hapiz update

--- app/Http/Controllers/HafizController.php
+++ app/Http/Controllers/HafizController.php
@@ -10,4 +10,19 @@ class HafizController extends Controller
     {
         return view('backend.hafiz.index');
     }
+    public function Proses(Request $request)
+    {
+        $a = $request->input('a');
+        $b = $request->input('b');
+        $r2 = $request->input('r2');
+
+        // menentukan persamaan lingkaran dengan pusat 
+        $hasil = " ($a * $a) + ($b * $b) = 0 ";
+        $hasila = " pow($x * $a) + pow($y * $b) "
+
+
+
+        return redirect('/bintang')->with('info', 'Hasil nya adalah :' .$hasil)->with('info2', 'Hasil nya adalah :' .$total_seluruh);
+
+    }
 }
--- resources/views/backend/hafiz/index.blade.php
+++ resources/views/backend/hafiz/index.blade.php
@@ -1,78 +1,110 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>hafiz</title>
-  </head>
-  <body >
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <title>Bintang</title>
+</head>
+
+<body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">PROYEK MTK</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/yusup">Yusup</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/hafiz">Hafiz</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/bintang">Bintang</a>
-        </li>
-        
-      </ul>
-      
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">PROYEK MTK</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/yusup">Yusup</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/hafiz">Hafiz</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/bintang">Bintang</a>
+          </li>
+
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+
+  <!-- ---------------------------------------- -->
+  <div class="container py-5">
+    <form action="/bintang_proses" method="POST">
+      @csrf
+
+      <h1>Garis Singgung Lingkaran</h1>
+      <div class="form-row">
+      </div>
+      <div class="form-group">
+      </div>
+      <div class="form-group">
+      </div>
+      <div class="form-row">
+        <div class="form-group col-md-2">
+          <input type="text" class="form-control" id="inputCity" name="a" placeholder="a">
+        </div>
+        <div class="form-group col-md-0">
+          <h4> + </h4>
+        </div>
+        <div class="form-group col-md-2">
+          <input type="text" class="form-control" id="inputCity" name="b" placeholder="b">
+        </div>
+        <div class="form-group col-md-0">
+          <h4> = </h4>
+        </div>
+        <div class="form-group col-md-2">
+          <input type="text" class="form-control" id="inputZip" name="r2" placeholder="r2" Required>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="form-check">
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Proses</button>
+    </form>
+
+    <div class="pt-3">
+      <div class="md-3">
+      </div>
+      <div class="md-3">
+        @if(session('info'))
+        <div class="alert alert-info">
+          {{ session('info')}}
+        </div>
+        @endif
+      </div>
+      <div class="md-3">
+        @if(session('info2'))
+        <div class="alert alert-info">
+          {{ session('info2')}}
+        </div>
+        @endif
+      </div>
     </div>
   </div>
-</nav>
 
-    <div class="container py-5">
-    <h1>(tugas)</h1>
-    <form>
-  <div class="mb-3">
-    <label class="form-label">Input nilai 1</label>
-    <input type="text" class="form-control" aria-describedby="emailHelp">
+
   </div>
-  <div class="mb-3">
-    <label class="form-label">input nilai 2</label>
-    <input type="text" class="form-control" >
-  </div>
-  <button type="submit" class="btn btn-primary">Proses</button>
-</form>
-<div class="py-5">
-<form>
-  <fieldset disabled>
-    <legend>Hasil</legend>
-    <div class="mb-3">
-      <input type="text" id="disabledTextInput" class="form-control">
-    </div>
-  </fieldset>
-</form>
-</div>
-    </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  <!-- --------------------------------------------- -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    -->
-  </body>
 </html>
