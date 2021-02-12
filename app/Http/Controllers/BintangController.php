987dbf568b702e8cf52b18fbe20c86703fb87ff2
yusupsupriyadi <yusupsupriyadi242@gmail.com>
2021-02-12 15:55:00 +0700

    update rumus

--- app/Http/Controllers/BintangController.php
+++ app/Http/Controllers/BintangController.php
@@ -14,21 +14,22 @@ class BintangController extends Controller
     public function Proses(Request $request)
     {
         $a = $request->input('a');
-        $b = $request->input('a');
+        $b = $request->input('b');
         $r2 = $request->input('r2');
 
         // menentukan persamaan lingkaran dengan pusat O (0,0)
-        $hasil = " ($x1 * $x1) + ($y1 * $y1) = 0 ";
+        $hasil = " ($a * $a) + ($b * $b) = 0 ";
 
         // menentukan titik pusat dan jari-jari
-        $hasila = -a * a;
-        $hasilb = -b * b;
+        $hasila = -$a * $a;
+        $hasilb = -$b * $b;
 
-        $hasilab = $hasil + $hasilb;
+        $hasilab = $hasila + $hasilb;
         $total = $hasilab - $r2;
-        $total_seluruh = "x2 + y2 $hasil_a + $hasil_b + $total = 0"
+        $total_seluruh = "x2 + y2 $hasila + $hasilb + $total = 0";
 
 
-        return redirect('/bintang')->with('info', 'Hasil nya adalah :' . $hasil)->with('info', 'Hasil nya adalah :' . $total_seluruh);
+        return redirect('/bintang')->with('info', 'Hasil nya adalah :' .$hasil)->with('info2', 'Hasil nya adalah :' .$total_seluruh);
+
     }
 }
